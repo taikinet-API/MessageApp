1860b8ba1895da3e047296b02f4504dc93f3dea0
taiki <yamatai140528@gmail.com>
2025-11-17 13:49:31 +0900

    Initial Commit

---
+++ messageApp/messageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  messageAppApp.swift
+//  messageApp
+//
+//  Created by mishima_lab     on 2025/11/17.
+//
+
+import SwiftUI
+
+@main
+struct messageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
